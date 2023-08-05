844ec3c0ac5a1b38ea0717214457bcb1473b48c7
Brodi Elwood <brodielwood@gmail.com>
2023-08-05 03:11:54 -0400

    tweaks to publishing script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,10 +17,12 @@ BUILD_OUTPUT=$(poetry build)
 # Check if upload URL was passed as an argument
 if [ $# -eq 1 ]; then
     # Extract the name of the .whl file
-    WHL_FILE=$(echo "${BUILD_OUTPUT}" | grep '.whl' | awk '{print $2}')
+  WHL_FILE=$(echo "${BUILD_OUTPUT}" | grep -oP 'Built.*\.whl' | awk '{print $2}')
 
     # Construct the full path to the .whl file
     WHL_FILE_PATH="${PWD}/dist/${WHL_FILE}"
+
+    echo $WHL_FILE_PATH
 
     # Upload
     curl -F expiration=1hour -F file=@${WHL_FILE_PATH} -X POST -w '%{redirect_url}\n' $1
