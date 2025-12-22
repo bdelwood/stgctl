792fe66d0b3ab93aa245e9f4cd4c6e0ebcf4bdbc
Brodi Elwood <brodielwood@gmail.com>
2025-12-22 16:20:35 -0500

    migrate to uv

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,23 +1,10 @@
-# Get version from git describe
-GIT_VERSION=$(git describe)
-
-# Format the version to be PEP440 compliant
-PEP440_VERSION=$(echo $GIT_VERSION | sed -e "s/^v//" -e "s/-/\.dev/" -e "s/-g/+/")
-
-# Update and build with poetry
-poetry version $PEP440_VERSION
-
-# Stage and amend commit
-git add pyproject.toml
-git commit --amend --no-edit
-
 # Build project
-BUILD_OUTPUT=$(poetry build)
+BUILD_OUTPUT=$(uv build)
 
 # Check if upload URL was passed as an argument
 if [ $# -eq 1 ]; then
     # Extract the name of the .whl file
-  WHL_FILE=$(echo "${BUILD_OUTPUT}" | grep -oP 'Built.*\.whl' | awk '{print $2}')
+    WHL_FILE=$(echo "${BUILD_OUTPUT}" | grep -oP 'Successfully built.*\.whl' | awk '{print $2}')
 
     # Construct the full path to the .whl file
     WHL_FILE_PATH="${PWD}/dist/${WHL_FILE}"
